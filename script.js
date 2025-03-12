490c2a36da96e24b4dc8f4bd8177c8e367351460
Abdoul Djelil Sana <sanaabdouldjelil5@gmail.com>
2025-03-12 18:30:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -251,3 +251,141 @@ if(userSelect==="Km to m"){
     message.innerHTML="Convertion <span style='color:red'>"+userSelect+"</span> indisponible pour l'instant"
 }
 }
+
+//Remplissage Tables
+function S1(){
+    let trT=document.getElementById("trT")
+    trT.classList.remove
+    //legende
+    let Tite=document.getElementById("Tite");
+    Tite.innerHTML="SEMESTRE 5";
+    //title
+    let tlt=document.getElementById("tlt");
+    tlt.innerHTML='CONNAISSANCES FONDAMENTALES';
+    //LIGNE 1
+    let UE1=document.getElementById("UE1");
+    UE1.innerHTML='Outils Mathématiques';
+    let ECUE1=document.getElementById("ECUE1");
+    ECUE1.innerHTML="Mesure et Intégration<br>Analyse Complexe et calculs différentiels<br>Algèbre/Analyse tensorielle";
+    let créditECUE1=document.getElementById("créditECUE1");
+    créditECUE1.innerHTML='2<br>2<br>2';
+    let créditUE1=document.getElementById("créditUE1");
+    créditUE1.innerHTML='6'
+    //LIGNE 2
+    let UE2=document.getElementById("UE2");
+    UE2.innerHTML='Probabilités-Statistiques';
+    let ECUE2=document.getElementById("ECUE2");
+    ECUE2.innerHTML="Probabilités de l'ingénieur<br>Statistiques de l'Ingénieur";
+    let créditECUE2=document.getElementById("créditECUE2");
+    créditECUE2.innerHTML='1<br>1';
+    let créditUE2=document.getElementById("créditUE2");
+    créditUE2.innerHTML='2'
+        //LIGNE 3
+        let UE3=document.getElementById("UE3");
+        UE3.innerHTML='Informatique 1';
+        let ECUE3=document.getElementById("ECUE3");
+        ECUE3.innerHTML="Bureautique-Excel<br>Programmation python";
+        let créditECUE3=document.getElementById("créditECUE3");
+        créditECUE3.innerHTML='2<br>2';
+        let créditUE3=document.getElementById("créditUE3");
+        créditUE3.innerHTML='4'
+            //title
+let tlt2=document.getElementById("tlt2");
+tlt2.innerHTML='';
+    //LIGNE 1
+    let UE4=document.getElementById("UE4");
+    UE4.innerHTML='';
+    let ECUE4=document.getElementById("ECUE4");
+    ECUE4.innerHTML="";
+    let créditECUE4=document.getElementById("créditECUE4");
+    créditECUE4.innerHTML='';
+    let créditUE4=document.getElementById("créditUE4");
+    créditUE4.innerHTML=''
+        //Ligne footer
+        let presuivant=document.getElementById("presuivant");
+        presuivant.innerHTML='<button class="navbutton" onclick="suivant0()">>>>></button>'
+}let itera=0;
+function affiche(){
+    let trT=document.getElementById("trT")
+    let trt=document.getElementById("trt")
+    let tr1=document.getElementById("tr1")
+    let tr2=document.getElementById("tr2")
+    let tr3=document.getElementById("tr3")
+    let trt2=document.getElementById("trt2")
+    let tr4=document.getElementById("tr4")
+    let bas=document.getElementById("bas")
+    if(itera===0){
+    trT.classList.remove('collapse')
+    trt.classList.remove('collapse')
+    tr1.classList.remove('collapse')
+    tr2.classList.remove('collapse')
+    tr3.classList.remove('collapse')
+    trt2.classList.remove('collapse')
+    tr4.classList.remove('collapse')
+    bas.classList.remove('collapse')
+    itera++
+    }else{
+        trT.classList.add('collapse')
+        trt.classList.add('collapse')
+        tr1.classList.add('collapse')
+        tr2.classList.add('collapse')
+        tr3.classList.add('collapse')
+        trt2.classList.add('collapse')
+        tr4.classList.add('collapse')
+        bas.classList.add('collapse')
+        itera--
+    }
+}
+document.getElementById('S1').onclick = function() {
+    affiche();
+    S1();
+};
+function suivant0(){
+    //title
+let tlt=document.getElementById("tlt");
+tlt.innerHTML='45';
+    //LIGNE 1
+    let UE1=document.getElementById("UE1");
+    UE1.innerHTML='Mécanique des milieux déformables';
+    let ECUE1=document.getElementById("ECUE1");
+    ECUE1.innerHTML="Mécanique des milieux continus<br>Resistance des matériaux";
+    let créditECUE1=document.getElementById("créditECUE1");
+    créditECUE1.innerHTML='2<br>3';
+    let créditUE1=document.getElementById("créditUE1");
+    créditUE1.innerHTML='5'
+    //LIGNE 2
+    let UE2=document.getElementById("UE2");
+    UE2.innerHTML='Mécanique et Energétique';
+    let ECUE2=document.getElementById("ECUE2");
+    ECUE2.innerHTML="Mécanique analytique<br>Stransfert thermique";
+    let créditECUE2=document.getElementById("créditECUE2");
+    créditECUE2.innerHTML='3<br>3';
+    let créditUE2=document.getElementById("créditUE2");
+    créditUE2.innerHTML='6'
+    //LIGNE 3
+    let UE3=document.getElementById("UE3");
+    UE3.innerHTML='Description et calcul de base';
+    let ECUE3=document.getElementById("ECUE3");
+    ECUE3.innerHTML="Description coque de navire<br>Calcul de Structure du Navire";
+    let créditECUE3=document.getElementById("créditECUE3");
+    créditECUE3.innerHTML='2<br>2';
+    let créditUE3=document.getElementById("créditUE3");
+    créditUE3.innerHTML='4'
+    //title
+let tlt2=document.getElementById("tlt2");
+tlt2.innerHTML='UE  CULTURE GENERALE';
+    //LIGNE 1
+    let UE4=document.getElementById("UE4");
+    UE4.innerHTML='Langue vivante';
+    let ECUE4=document.getElementById("ECUE4");
+    ECUE4.innerHTML="Anglais Technique 1<br>Anglais général<br>Technique d'expression";
+    let créditECUE4=document.getElementById("créditECUE4");
+    créditECUE4.innerHTML='1<br>1<br>1';
+    let créditUE4=document.getElementById("créditUE4");
+    créditUE4.innerHTML='3'
+    //Ligne footer
+    let presuivant=document.getElementById("presuivant");
+    presuivant.innerHTML='<button class="navbutton" onclick="S1()"><<<<</button>'
+}
+
+//S2
