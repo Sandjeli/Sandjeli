6bd2b851ab771426c79873baf58ef875e2956adc
Abdoul Djelil Sana <sanaabdouldjelil5@gmail.com>
2026-01-08 19:33:22 +0000

    Update projets.js

--- Robot/projets.js
+++ Robot/projets.js
@@ -1,6 +1,7 @@
 // Tableau de projets
 
 // Debut Basics
+// Debut Basics
 const Basics = [
   {
     titre: "Clignotement LED",
@@ -11,18 +12,17 @@ const Basics = [
     github: "https://github.com/tonpseudo/led",
     video: "https://www.youtube.com/watch?v=example"
   },
-
   {
-  titre: "Contrôle d'accès RFID",
-  description: "Système RFID pour allumer une LED selon l'UID d'une carte.",
-  image: "./images/RFID.PNG",
-  longDescription: "Ce projet met en œuvre un système de contrôle d'accès utilisant un module RFID MFRC522 et un Arduino. Lorsqu'une carte RFID est approchée, son UID est lu et comparé à un UID autorisé. Si la carte est reconnue, une LED verte s'allume pour indiquer l'accès autorisé ; sinon, une LED rouge signale un accès refusé. Ce projet illustre la lecture de cartes RFID, la comparaison de données, et le contrôle de sorties électroniques via Arduino. Il constitue une base pédagogique pour les applications de sécurité, les systèmes de porte automatisés et l'apprentissage de l'intégration matériel-logiciel.",
-  technos: ["Arduino UNO", "Module RFID MFRC522", "LED RGB", "Résistances 220Ω", "Câbles de connexion"],
-  github: "./Fichiers/RFID_LED_Code.pdf",
-  video: "./Fichiers/RFID_LED_Demo.mp4"
-}
-
+    titre: "Contrôle d'accès RFID",
+    description: "Système RFID pour allumer une LED selon l'UID d'une carte.",
+    image: "./images/RFID.PNG",
+    longDescription: "Ce projet met en œuvre un système de contrôle d'accès utilisant un module RFID MFRC522 et un Arduino. Lorsqu'une carte RFID est approchée, son UID est lu et comparé à un UID autorisé. Si la carte est reconnue, une LED verte s'allume pour indiquer l'accès autorisé ; sinon, une LED rouge signale un accès refusé. Ce projet illustre la lecture de cartes RFID, la comparaison de données, et le contrôle de sorties électroniques via Arduino. Il constitue une base pédagogique pour les applications de sécurité, les systèmes de porte automatisés et l'apprentissage de l'intégration matériel-logiciel.",
+    technos: ["Arduino UNO", "Module RFID MFRC522", "LED RGB", "Résistances 220Ω", "Câbles de connexion"],
+    github: "./Fichiers/RFID_LED_Code.pdf",
+    video: "./Fichiers/RFID_LED_Demo.mp4"
+  }
 ];
+
 
 // Debut Intermediaire
 const Intermediaires = [
@@ -51,12 +51,11 @@ const Avances = [
   }
 ];
 
-
-
+// Sélection du container HTML
 const containerBasic = document.getElementById("projets-containerBasic");
 
-// Ajouter les projets
-projetsBasics.forEach(projet => {
+// Ajouter les projets dans le container
+Basics.forEach(projet => {
   const card = document.createElement("div");
   card.classList.add("col-4", "mb-3");
 
