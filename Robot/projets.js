73498524813446a286976417aa598bf783021d39
Abdoul Djelil Sana <sanaabdouldjelil5@gmail.com>
2026-01-08 19:48:16 +0000

    Update projets.js

--- Robot/projets.js
+++ Robot/projets.js
@@ -15,7 +15,7 @@ const Basics = [
   {
     titre: "Contrôle d'accès RFID",
     description: "Système RFID pour allumer une LED selon l'UID d'une carte.",
-    image: "./images/RFID.PNG",
+    image: "./images/RFID.png",
     longDescription: "Ce projet met en œuvre un système de contrôle d'accès utilisant un module RFID MFRC522 et un Arduino. Lorsqu'une carte RFID est approchée, son UID est lu et comparé à un UID autorisé. Si la carte est reconnue, une LED verte s'allume pour indiquer l'accès autorisé ; sinon, une LED rouge signale un accès refusé. Ce projet illustre la lecture de cartes RFID, la comparaison de données, et le contrôle de sorties électroniques via Arduino. Il constitue une base pédagogique pour les applications de sécurité, les systèmes de porte automatisés et l'apprentissage de l'intégration matériel-logiciel.",
     technos: ["Arduino UNO", "Module RFID MFRC522", "LED RGB", "Résistances 220Ω", "Câbles de connexion"],
     github: "./Fichiers/RFID_LED_Code.pdf",
@@ -71,6 +71,7 @@ Basics.forEach(projet => {
       <div class="card-footer">
         <a href="${projet.github}" class="btn btn-primary" target="_blank">Voir le code</a>
         <a href="${projet.video}" class="btn btn-secondary" target="_blank">Voir la vidéo</a>
+        <a href="projet.html" class="btn btn-secondary" target="_blank">plus de detailles</a>
       </div>
     </div>
   `;
